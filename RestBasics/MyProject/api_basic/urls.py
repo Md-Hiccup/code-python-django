022a677778bad99df489adcccfeef3be8c995148
Hussain <ask2md@gmail.com>
2020-09-13 14:34:15 +0530

    added viewsets

--- RestBasics/MyProject/api_basic/urls.py
+++ RestBasics/MyProject/api_basic/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetails, GenericAPIView, GenericDetailAPIView
+from django.urls import path, include
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails, GenericAPIView, GenericDetailAPIView, ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
     # function based view
@@ -7,10 +11,14 @@ urlpatterns = [
     path('detail/<int:pk>/', article_detail),
 
     # class based view
-    path('class/article/', ArticleAPIView.as_view(), name='article'),
-    path('class/detail/<int:id>/', ArticleDetails.as_view(), name='detail'),
+    path('class/article/', ArticleAPIView.as_view()),
+    path('class/detail/<int:id>/', ArticleDetails.as_view()),
 
     # generic based view
-    path('generic/article/', GenericAPIView.as_view(), name='generic'),
-    path('generic/article/<int:id>/', GenericDetailAPIView.as_view(), name='generic'),
+    path('generic/article/', GenericAPIView.as_view()),
+    path('generic/article/<int:id>/', GenericDetailAPIView.as_view()),
+
+    # viewsets
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>', include(router.urls)),
 ]
